36359a29ebad91b958197f2c5249501591676c77
Drenych742 <drain2008@bk.ru>
2024-10-14 09:31:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+url_sonar = 'http://sonarqube.example:8081/'
+sonar_token = 'squ_5xxxxxxxxe8b55782dxxxx353c10bbdfxxxbf96'
+gitlab_url = 'http://gitlab.example/'
+gitlab_token = 'glpat-HVmPTy-xxxxxxxxxx'
+log_filename='/home/user/log.log'
+list_git_groups = [
+    '1project', 
+    '2project'
+    ]
